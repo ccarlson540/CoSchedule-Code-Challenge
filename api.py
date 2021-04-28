6e4f6e623a4008501d3ad050b2273149bec3e971
ccarlson540 <57463207+ccarlson540@users.noreply.github.com>
2021-04-28 18:57:27 -0500

    API backend in Python/Flask
    Created API backend in Python and Flask because of familiarity with Python. Learning Flask as I go through this challenge. Successfully connects to Reddit API. Moved index.html to templates folder. Created logins.csv to store state strings on login.

---
+++ api.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 28 16:27:42 2021
+
+@author: Charlie Carlson
+"""
+
+from flask import Flask, abort, request
+from uuid import uuid4
+import requests
+import requests.auth
+import urllib
+import pandas as pd
+import flask
+
+CLIENT_ID = 'CO_3bjGxqJkH0A'
+CLIENT_SECRET = 'pHK5oLHVTIlRdxHEAM_ASarbtG3x8w'
+REDIRECT_URI = 'http://localhost:65010/reddit_callback'
+BASE_URL = 'http://localhost:65010/'
+
+
+def user_agent():
+    return 'CoSchedule Code Challenge by /uWholeSet4125'
+
+def base_headers():
+    return {"User-Agent": user_agent()}
+
+
+app = Flask(__name__)
+@app.route('/')
+def homepage():
+    #TODO hide behind my site's login
+    #TODO create login html page (use index.html as base file)
+    #TODO remove direct link and use as redirect after logging in/make page
+    # explaining why second authentication to Reddit is required
+    text = '<a href="%s">Authenticate with reddit</a>'
+    return text % make_authorization_url()
+
+
+def make_authorization_url():
+    # Generate a random string for the state parameter
+    # Save it for use later to verify user logging in with same state
+    state = str(uuid4())
+    save_created_state(state)
+    params = {"client_id": CLIENT_ID,
+              "response_type": "code",
+              "state": state,
+              "redirect_uri": REDIRECT_URI,
+              "duration": "temporary",
+              "scope": "identity"}
+    url = ('https://www.reddit.com/api/v1/authorize?client_id=' + CLIENT_ID
+    + '&response_type=code&state=' + state +'&redirect_uri=' + REDIRECT_URI
+    + '&duration=temporary&scope=read' )
+    return url
+
+# Save results to csv as basic database
+#TODO determine if username and time should be saved to database also
+def save_created_state(state):
+    logins = pd.read_csv('logins.csv')
+    logins = logins.append({'state' : state}, ignore_index = True)
+    logins.to_csv('logins.csv')
+    
+# check if state is contained in database
+def is_valid_state(state):
+    logins = pd.read_csv('logins.csv')
+    if state in list(logins['state']):
+        return True
+    else:
+        return False
+
+@app.route('/reddit_callback')
+def reddit_callback():
+    error = request.args.get('error', '')
+    if error:
+        return "Error: " + error
+    state = request.args.get('state', '')
+    if not is_valid_state(state):
+        # login is invalid: return HTTP 403 - Forbidden
+        abort(403)
+    code = request.args.get('code')
+    access_token = get_token(code)
+    # TODO: pass access token on redirect
+    return flask.redirect(flask.url_for('search_page'))
+
+def get_token(code):
+    client_auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
+    post_data = {"grant_type": "authorization_code",
+                 "code": code,
+                 "redirect_uri": REDIRECT_URI}
+    headers = base_headers()
+    response = requests.post("https://ssl.reddit.com/api/v1/access_token",
+                             auth=client_auth,
+                             headers=headers,
+                             data=post_data)
+    token_json = response.json()
+    return token_json["access_token"]
+
+@app.route('/search/')
+def search_page():
+    return flask.render_template('index.html')
+    
+def get_username(access_token):
+    headers = base_headers()
+    headers.update({"Authorization": "bearer " + access_token})
+    response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
+    me_json = response.json()
+    return me_json['name']
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=65010)
